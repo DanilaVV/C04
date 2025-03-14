60624b20e17b070f5d2661660be18a79a473eaf4
DanilaVV <voznyuk.danila@yandex.ru>
2025-03-14 09:38:31 +0000

    commit3

---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+void	ft_condition(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (base[i])
+	{
+		j = i + 1;
+		if(base[i] == '+' || base[i] == '-')
+			return (0);
+		while (base[j])
+		{
+			if(base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (i < 2)
+		return (0);
+	return (1);
+}
+
+void	ft_putch(char c)
+{
+	write(1, &c, 1);
+}
+
+unsigned int ft_base(char *base)
+{
+	unsigned int i;
+
+	i = 0;
+	while (base[i])
+		i++;
+	return (i);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	i;
+	int	bs;
+
+	i = 0;
+	if (!ft_condition(base))
+		return ;
+	
+}
