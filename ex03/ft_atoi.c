e2a5d43e499719da3e75e2c51e8e847178c75fc0
DanilaVV <voznyuk.danila@yandex.ru>
2025-03-14 08:48:00 +0000

    commit2

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -13,7 +13,9 @@ int	checkch(char c)
 {
 	if (checknb(c))
 		return (1);
-	if (c == ' ' || c == '-' || c == '+')
+	if (c == ' ' || c == '-' || c == '+' || c == '\t')
+		return (1);
+	if (c == '\v' || c == '\n' || c == '\r' || c == 'f')
 		return (1);
 	return (0);
 }
@@ -27,7 +29,7 @@ int	checkstr(char *str)
 	ch = 0;
 	while (str[i] && checkch(str[i]))
 	{
-		if ((str[i - 1] == '-' || str[i - 1] == '+') && str[i] == ' ')
+		if ((str[i - 1] == '-' || str[i - 1] == '+') && str[i] <= 32)
 			return (0);
 		if (checknb(str[i]))
 			ch++;
@@ -84,7 +86,7 @@ int	ft_atoi(char *str)
 }
 /*int	main(void)
 {
-	printf("%d\n", ft_atoi(" --2147483648"));
-	printf("%d\n", atoi("2147483648"));
+	printf("%d\n", ft_atoi(" --++--2147483648sdf"));
+	printf("%d\n", atoi("2147483648sdf"));
 	return (0);
 }*/
