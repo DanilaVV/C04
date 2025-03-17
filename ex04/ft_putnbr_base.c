a83597e86357b9b10b6c9cc8519e7ee887fa5d4a
DanilaVV <voznyuk.danila@yandex.ru>
2025-03-17 06:32:56 +0000

    commit6

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-void	ft_condition(char *base)
+int	ft_condition(char *base)
 {
 	int	i;
 	int	j;
@@ -9,11 +9,11 @@ void	ft_condition(char *base)
 	while (base[i])
 	{
 		j = i + 1;
-		if(base[i] == '+' || base[i] == '-')
+		if (base[i] == '+' || base[i] == '-')
 			return (0);
 		while (base[j])
 		{
-			if(base[i] == base[j])
+			if (base[i] == base[j])
 				return (0);
 			j++;
 		}
@@ -24,14 +24,20 @@ void	ft_condition(char *base)
 	return (1);
 }
 
-void	ft_putch(char c)
+void	ft_putch(int n)
 {
-	write(1, &c, 1);
+	char	c;
+
+	if (n < 10)
+		c = n + '0';
+	else
+		c = 'A' + n - 10;
+	write (1, &c, 1);
 }
 
-unsigned int ft_base(char *base)
+int	ft_base(char *base)
 {
-	unsigned int i;
+	int	i;
 
 	i = 0;
 	while (base[i])
@@ -39,17 +45,32 @@ unsigned int ft_base(char *base)
 	return (i);
 }
 
+int	ft_abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int	i;
 	int	bs;
 
-	i = 0;
 	if (!ft_condition(base))
 		return ;
-	
+	bs = ft_base(base);
+	if (nbr < 0)
+		write(1, "-", 1);
+	if (nbr != -2147483648 && ft_abs(nbr) < bs)
+	{
+		ft_putch(ft_abs(nbr));
+		return ;
+	}
+	ft_putnbr_base(ft_abs(nbr / bs), base);
+	ft_putnbr_base(ft_abs(nbr % bs), base);
 }
 /*int	main(void)
 {
+	ft_putnbr_base(0, "01234567");
 	return (0);
 }*/
